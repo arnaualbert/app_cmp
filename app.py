6be5a22c072eccc47309a2acc084413515df3ff3
Arnau Albert <arnaualbert@pop-os.localdomain>
2023-02-22 09:16:08 +0100

    v1

--- app.py
+++ app.py
@@ -70,11 +70,11 @@ def demultiplexing():
                 
                 print(f.filename)
                 print(secure_filename(f.filename))
-                # filename = secure_filename(f.filename)
-                # f.save(os.path.join(app.config['DEMULTIPLEXING_FWD_FOLDER'], filename))
-                # fastas_fwd_ls.append(os.path.join(app.config['DEMULTIPLEXING_FWD_FOLDER'],filename))
-                # fastas_fwd_ls.append(os.path.join(filename))
-                fastas_fwd_ls.append(os.path.join(output_dir,f.filename))
+                filename = secure_filename(f.filename)
+                f.save(os.path.join(app.config['DEMULTIPLEXING_FWD_FOLDER'], filename))
+                fastas_fwd_ls.append(os.path.join(app.config['DEMULTIPLEXING_FWD_FOLDER'],filename))
+                #fastas_fwd_ls.append(os.path.join(filename))
+                #fastas_fwd_ls.append(os.path.join(output_dir,f.filename))
 
         fastas_rv = request.files.getlist("fastas_rv")
         fastas_rv_ls = []
@@ -129,6 +129,64 @@ def demultiplexing():
 
 @app.route('/demultiplexing_batch')
 def demultiplexing_batch():
+    if request.method == 'POST':
+        output_dir = request.form['output_dir']
+        fastas_fwd = request.files.getlist("fastas_fwd")
+        fastas_fwd_ls = []
+        for f in fastas_fwd:
+            if f and allowed_file(f.filename):
+                # print(f'./{f.filename}')
+                # print(f.filename)
+                
+                print(f.filename)
+                print(secure_filename(f.filename))
+                # filename = secure_filename(f.filename)
+                # f.save(os.path.join(app.config['DEMULTIPLEXING_FWD_FOLDER'], filename))
+                # fastas_fwd_ls.append(os.path.join(app.config['DEMULTIPLEXING_FWD_FOLDER'],filename))
+                # fastas_fwd_ls.append(os.path.join(filename))
+                fastas_fwd_ls.append(os.path.join(output_dir,f.filename))
+
+        fastas_rv = request.files.getlist("fastas_rv")
+        fastas_rv_ls = []
+        for f in fastas_rv:
+            if f and allowed_file(f.filename):
+                filename = secure_filename(f.filename)
+                print(f.filename)
+                ###############################################################################################
+                # print(f.name)
+                # f.save(os.path.join(app.config['DEMULTIPLEXING_RV_FOLDER'], filename))
+                # fastas_rv_ls.append(os.path.join(app.config['DEMULTIPLEXING_RV_FOLDER'],filename))
+                ###############################################################################################
+                fastas_rv_ls.append(os.path.join(output_dir,f.filename))
+
+        # output_dir = request.form['output_dir']
+        ref_genome = request.form.getlist('ref_genome')
+        organism_name = request.form.getlist('organism_name')
+        num_of_threads = request.form['num_of_threads']
+        reads_per_chunk = request.form['reads_per_chunk']
+        #replace = request.form['replace']
+        skip_removing_tmp_files = request.form['skip_removing_tmp_files']
+        wit_db = request.form['wit_db']
+        # params = {"fastas_fwd":fastas_fwd_ls,"fastas_rv":fastas_rv_ls,"output_dir":output_dir,"ref_genome":ref_genome,"organism_name":organism_name,"num_of_threads":num_of_threads,"reads_per_chunk":reads_per_chunk,"skip_removing_tmp_files":skip_removing_tmp_files,"wit_db":wit_db}
+        params = {"--fastq1":fastas_fwd_ls,"--fastq2":fastas_rv_ls,"--outdir":output_dir,"--refGenomes":ref_genome,"--sampleNames":organism_name,"--trheads":num_of_threads,"--nreads_per_chunk":reads_per_chunk,"--skip_removing_tmp_files":skip_removing_tmp_files,"--wit_db":wit_db}
+        print(params)
+        print(type(params))
+        #for rout in fastas_fwd_ls:
+        fastas_fs_ls_string = " ".join(fastas_fwd_ls)
+        fastas_rv_ls_string = " ".join(fastas_rv_ls)
+        ref_genome_string = " ".join(ref_genome)
+        organism_name_string = " ".join(organism_name)
+        command = f' --fastq1 {fastas_fs_ls_string} --fastq2 {fastas_rv_ls_string} --outdir {output_dir} --refGenomes {ref_genome_string} --sampleNames {organism_name_string} --trheads {num_of_threads} --nreads_per_chunk {reads_per_chunk} --skip_removing_tmp_files {skip_removing_tmp_files} --wit_db {wit_db}'
+        print(command)
+        print(type(command))
+
+        #with open('commands/command.txt', 'w') as f:
+        #    f.write(command)
+        
+        #print(f'fastas_fwd: {fastas_fwd_ls}, fastas_rv: {fastas_rv_ls}, output_dir: {output_dir}, ref_genome: {ref_genome}, organism_name: {organism_name},num_of_threads: {num_of_threads}, reads_per_chunk: {reads_per_chunk}, replace: {replace},skip_removing_tmp_files: {skip_removing_tmp_files}, wit_db: {wit_db}')
+        # return redirect(url_for('demultiplexing'))
+        data = {'command':command}
+        return render_template('command.html',data=data)
     return render_template('demultiplexing_batch.html')
 
 #############################################################################################################################
